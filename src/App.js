2482c11f70e304eec35815c9725f6a05b34bd434
shashankbansal023 <shashankbansal023@gmail.com>
2022-03-17 18:34:31 +0530

    details-page-created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import './App.css'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Details from './pages/Details'
+import './App.css'
 
 function App() {
 
-  
   return (
-       <Home/>
+    <div className="app">
+          <Router>
+              <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/details/:user" element={<Details></Details>}/>
+              </Routes>
+          </Router>
+    </div>
   )
 }
 
--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -1,22 +1,55 @@
 import React from 'react'
-import Rows from './Rows'
+import { useNavigate } from 'react-router-dom';
 
 const CustomTable=({headers , columns , rows})=>{
 
     console.log(headers , columns , rows);
+    const navigate = useNavigate();
+
+    function saveUserData(data){
+        let user = localStorage.getItem('user');
+        if(user){
+            localStorage.clear();
+        }
+        localStorage.setItem ('user' , JSON.stringify(data))
+    }
+
+
+    function handleClick(name,row){
+        navigate(`/details/${name}`);
+        saveUserData(row);
+    }
+
     return (
         <table>
             <thead>
                 <tr>
                     {
-                        headers.map(header=>{
-                            return <th>{header}</th>
+                        headers.map((header,id)=>{
+                            return <th key={id}>{header}</th>
                         })
                     }
                 </tr>
             </thead>
             <tbody>
-                <Rows rows={rows} columns={columns}/>
+                {
+                    rows.map(row=>{
+                        
+                        const {first_name , last_name , age , email ,web} = row;
+
+                        return (
+                            <tr>
+                                <td onClick={()=>handleClick(first_name , row)}>{first_name}</td>
+                                <td>{last_name}</td>
+                                <td>{age}</td>
+                                <td>{email}</td>
+                                <td>
+                                    <a href={web} target="_blank"> {web} </a>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
             </tbody>
         </table>
     )
